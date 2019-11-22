6c8eac8efeb38f9580f5528632c90d9af53ea4cf
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-22 15:30:47 -0500

    fixed issue on models

--- tunr_project/tunr_app/models.py
+++ tunr_project/tunr_app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Session(models.Model):
-    input_image_path: models.CharField(max_length=255, default='n/a')
-    result_image_path: models.CharField(max_length=255, default='n/a')
-    percent_match: models.IntegerField()
-    target_emo: models.CharField(max_length=100, default='n/a')
+    user_picture = models.CharField(max_length=255, default="n/a")
+    emoji_image = models.CharField(max_length=255, default='n/a')
+    percent_match = models.IntegerField()
+    target_emo = models.CharField(max_length=100, default='n/a')
 
     def __str__(self):
-        return self.input_image_path
+        return self.user_picture
--- tunr_project/tunr_app/serializers.py
+++ tunr_project/tunr_app/serializers.py
@@ -7,5 +7,5 @@ from .models import Session
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
-        fields = ('input_image_path', 'result_image_path', 'percent_match', 'target_emo')
+        fields = ('user-picture', 'emoji-image', 'percent-match', 'target-emo')
 
