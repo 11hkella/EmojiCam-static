19a3e08bc081c69d5daf02f22451973feb96b53e
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-25 16:18:54 -0500

    successfully loaded emoji and model assets

--- client/src/component/EmojiCam.js
+++ client/src/component/EmojiCam.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react'
 
+// import nodejs bindings to native tensorflow,
+// not required, but will speed up things drastically (python required)
+//import '@tensorflow/tfjs-node';
+
+// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+import * as canvas from 'canvas';
+
+import * as faceapi from 'face-api.js';
+
+// patch nodejs environment, we need to provide an implementation of
+// HTMLCanvasElement and HTMLImageElement, additionally an implementation
+// of ImageData is required, in case you want to use the MTCNN
+const { Canvas, Image, ImageData } = canvas
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+
 export default class EmojiCam extends Component {
     state = {
         video: null,
@@ -24,13 +39,67 @@ export default class EmojiCam extends Component {
             height: video.height
         }
         this.setState({ video, displaySize })
+        this.loadAssets()
     }
+
+    loadImages = new Promise((resolve, reject) => {
+        try {
+            //happy
+            const happyImg = new Image()
+            happyImg.src = "./images/cool.png"
+            // neutral
+            const neutralImg = new Image()
+            neutralImg.src = "./images/neutral.png"
+            //angry
+            const angryImg = new Image()
+            angryImg.src = "./images/angry.png"
+            //disgust
+            const disgustImg = new Image()
+            disgustImg.src = "./images/disgust.png"
+            // fear
+            const fearImg = new Image()
+            fearImg.src = "./images/fear.png"
+            //sad
+            const sadImg = new Image()
+            sadImg.src = "./images/crying.png"
+            //surprise
+            const surpriseImg = new Image()
+            surpriseImg.src = "./images/surprised.png"
+
+            this.setState({
+                happyImg,
+                neutralImg,
+                angryImg,
+                disgustImg,
+                fearImg,
+                sadImg,
+                surpriseImg,
+            })
+            resolve('Images loaded');
+        }
+        catch {
+            reject('Image cannot load')
+        }
+    })
+
+    loadAssets = () => {
+        Promise.all([
+            faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+            faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+            faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+            this.loadImages
+        ]).then(() => {
+            console.log('models all loaded')
+        })
+    }
+
+
 
 
     render() {
         return (
-            <div class='emoji-cam' id='emojiCam'>
-                <video id='video' autoplay muted></video>
+            <div className='emoji-cam' id='emojiCam' >
+                <video id='video' autoPlay muted></video>
             </div>
         )
     }
