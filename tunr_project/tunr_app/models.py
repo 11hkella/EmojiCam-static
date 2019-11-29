365d25bff78fd82938b97041c04abf7da1ae72d1
Henry Kellam <henry.a.kellam@gmail.com>
2019-11-29 18:29:40 -0500

    fixed a model field issue in sessions model

--- tunr_project/tunr_app/models.py
+++ tunr_project/tunr_app/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Session(models.Model):
     latitude = models.DecimalField(max_digits=19, decimal_places=10)
     longitude = models.DecimalField(max_digits=19, decimal_places=10)
-    timestamp = models.DateField.auto_now_add()
+    timestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.timestamp
