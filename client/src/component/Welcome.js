5fdf0d074e9cc99c6a2d656e66cb165396a44814
Henry Kellam <henry.a.kellam@gmail.com>
2019-12-03 09:08:05 -0500

    fixed typo

--- client/src/component/Welcome.js
+++ client/src/component/Welcome.js
@@ -9,7 +9,7 @@ export default class Welcome extends Component {
             <div className='welcome-container'>
                 <h1>Welcome to EmojiCam!</h1>
 
-                <p>Turn your facial expression into an emoji. Utilizing the power of machine learning algorithms as well as pre-trained facial recognition models, EmojiCam has the power to locate facial features and detect expresions in real time! </p>
+                <p>Turn your facial expression into an emoji. Utilizing the power of machine learning algorithms as well as pre-trained facial recognition models, EmojiCam has the power to locate facial features and detect expressions in real time! </p>
             </div >
         )
     }
